b1f96dde7b8a1fd669cd75d99de119a20f042fc8
igortorres17 <igor.s.torres@outlook.com>
2021-10-06 08:37:03 -0300

    Implementando as entidades

---
+++ src/DevIO.Business/Models/Fornecedores/Enums/TipoFornecedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores.Enums
+{
+    public enum TipoFornecedor : int
+    {
+        PessoaFisica =1,
+        PessoaJuridica
+    }
+}
--- src/DevIO.Business/Models/Produtos/Produto.cs
+++ src/DevIO.Business/Models/Produtos/Produto.cs
@@ -1,9 +1,19 @@
 ﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Fornecedores;
+using System;
 
 namespace DevIO.Business.Models.Produtos
 {
     public class Produto : Entity
     {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+        public Guid FornecedorId { get; set; }
+        public Fornecedor Fornecedor { get; set; }
 
 
     }
