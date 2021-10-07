95121b37a153223e69a0efe0a0af74bd6120da80
igortorres17 <igor.s.torres@outlook.com>
2021-10-07 08:27:27 -0300

    Implementando interface de acesso a dados

---
+++ src/DevIO.Business/Models/Fornecedores/Interface/IFornecedorRepository.cs
@@ -0,0 +1,15 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores.Interface
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterEnderecoFornecedor(Guid id);
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+    }
+}
---
+++ src/DevIO.Business/Models/Produtos/Interface/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Produtos.Interface
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosFornecedor(Guid fornecedorId);
+        Task<IEnumerable<Produto>> ObterProdutoFornecedores();
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
