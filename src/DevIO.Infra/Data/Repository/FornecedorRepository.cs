8ce130eb7932ef03b7e5a164c146af87b24afca9
IgorTorres <itorres@provertec.com.br>
2021-10-25 15:28:31 -0300

    Implementando os repositórios específicos

---
+++ src/DevIO.Infra/Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DevIO.Infra.Data.Repository
+{
+    class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await context.Fornecedores.AsNoTracking()
+                .Include(f => f.Endereco)
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutoEndereco(Guid id)
+        {
+            return await context.Fornecedores.AsNoTracking()
+               .Include(f => f.Endereco)
+               .Include(f => f.Produtos)
+               .FirstOrDefaultAsync(f => f.Id == id);
+        }
+    }
+}
---
+++ src/DevIO.Infra/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,33 @@
+﻿using DevIO.Business.Models.Produtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DevIO.Infra.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await context.Produtos.AsNoTracking()
+                .Include(p => p.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await context.Produtos.AsNoTracking()
+                .Include(p => p.Fornecedor)
+                .OrderBy(p => p.Nome)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
