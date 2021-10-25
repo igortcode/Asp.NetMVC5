794ada266e091010d3a4ec172c6dd8e4262947a7
IgorTorres <itorres@provertec.com.br>
2021-10-25 16:52:23 -0300

    Implementando algumas validações com FluentValidation

--- src/DevIO.Business/Models/Fornecedores/Validations/FornecedorValidation.cs
+++ src/DevIO.Business/Models/Fornecedores/Validations/FornecedorValidation.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using DevIO.Business.Core.Validations.Documents;
 
 namespace DevIO.Business.Models.Fornecedores.Validations
 {
@@ -11,6 +12,18 @@ namespace DevIO.Business.Models.Fornecedores.Validations
                 .WithMessage("O campo {PropertyName} precisa ser fornecido")
                 .Length(2,200)
                 .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            When(f => f.TipoFornecedor == Enums.TipoFornecedor.PessoaFisica, () => {
+                RuleFor(f => ValidaCPF.IsCpf(f.Documento)).Equal(true).WithMessage("O documento fornecido é inválido");
+                
+                });
+
+
+            When(f => f.TipoFornecedor == Enums.TipoFornecedor.PessoaJuridica, () => {
+            RuleFor(f => ValidaCNPJ.IsCnpj(f.Documento)).Equal(true).WithMessage("O documento fornecido é inválido"));
+            });
         }
+
+
     }
 }
